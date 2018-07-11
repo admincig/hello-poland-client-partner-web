25344260a00c5fa86d8178e799d6cc41b24a1ed9
Janusz Przybylski <janusz@fream.pl>
2018-07-11 12:08:12 +0200

    HPL-258 Updated React Web App template

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,3 @@
-import { compose } from 'redux';
 import HomeView from 'views/HomeView';
-import withRoot from 'src/withRoot';
-import withRedux from 'services/redux/withRedux';
 
-export default compose(
-  withRedux(),
-  withRoot,
-)(HomeView);
+export default HomeView;
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,3 @@
-import { compose } from 'redux';
 import LoginView from 'views/LoginView';
-import withRoot from 'src/withRoot';
-import withRedux from 'services/redux/withRedux';
 
-export default compose(
-  withRedux(),
-  withRoot,
-)(LoginView);
+export default LoginView;
