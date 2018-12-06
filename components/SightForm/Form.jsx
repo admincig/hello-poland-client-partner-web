ccf3a32f01fe8457cfb78c0b55c5a5e5b1513fc7
wlyskawa <wlyskawa@fream.pl>
2018-12-06 11:16:10 +0100

    changed all 'wydarzenia' occurences

--- components/SightForm/Form.jsx
+++ components/SightForm/Form.jsx
@@ -317,7 +317,7 @@ class SightForm extends Component {
               {/* render={switchProps => ( */}
               {/* <FormControlLabel */}
               {/* control={<Switch {...fieldToSwitch(switchProps)} />} */}
-              {/* label="Dodaj wydarzenie ogólne" */}
+              {/* label="Dodaj ofertę ogólną" */}
               {/* /> */}
               {/* )} */}
               {/* /> */}
