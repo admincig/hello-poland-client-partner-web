c0fb6edd5d877ba3f47e6265501d5de110c4e3dc
Janusz Przybylski <janusz@fream.pl>
2019-01-10 13:20:33 +0100

    HPL-611 Blocking available tickets depending on limitation place

--- components/CalendarEventForm/CalendarEventForm.jsx
+++ components/CalendarEventForm/CalendarEventForm.jsx
@@ -187,6 +187,13 @@ class CalendarEventForm extends React.Component {
 
   isChecked = (data, element) => data && data.some(item => element === item);
 
+  hasTicketAvailabilityLimit = (ticketDefinitions) => {
+    return ticketDefinitions.some(({ availableTicketsNumber }) => (
+      Number.isInteger(availableTicketsNumber)
+      && availableTicketsNumber > 0
+    ));
+  };
+
   render() {
     const {
       classes, formData: initialFormData, onChange, readOnly,
@@ -220,7 +227,7 @@ class CalendarEventForm extends React.Component {
                 />
                 <TextField
                   fullWidth
-                  disabled={readOnly}
+                  disabled={readOnly || this.hasTicketAvailabilityLimit(formData.ticketDefinitions)}
                   label="Limit biletów w puli"
                   margin="normal"
                   name="availableTicketsNumber"
@@ -483,6 +490,10 @@ class CalendarEventForm extends React.Component {
                     }
                     {formData.ticketDefinitions &&
                       <TicketDefinitionList
+                        disableAvailability={
+                          Number.isInteger(formData.availableTicketsNumber)
+                          && formData.availableTicketsNumber > 0
+                        }
                         ticketDefinitions={formData.ticketDefinitions}
                         ticketDefinitionsList={ticketDefinitionsList}
                         onAvailabilityChange={handleTicketDefinitionChange}
