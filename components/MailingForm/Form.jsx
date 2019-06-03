7971baa1a00ebaf511c7919f91cb609b8f4ecea8
btuchowski <btuchowski@fream.pl>
2019-06-03 15:15:15 +0200

    changes after cr

--- components/MailingForm/Form.jsx
+++ components/MailingForm/Form.jsx
@@ -59,26 +59,16 @@ class MailingForm extends Component {
   };
 
   handleSubmitSuccess = actions => () => {
-    const { onSubmitSuccess, onClearError, clearError } = this.props;
+    const { onSubmitSuccess, clearError } = this.props;
 
     if (onSubmitSuccess) {
       onSubmitSuccess(actions);
-      if (onClearError) {
-        onClearError();
-      } else {
-        clearError();
-      }
+
       return;
     }
 
     const { resetForm, setSubmitting } = actions;
-
-    if (onClearError) {
-      onClearError();
-    } else {
-      clearError();
-    }
-
+    clearError();
     setSubmitting(false);
     resetForm();
   };
@@ -119,7 +109,6 @@ MailingForm.propTypes = {
   clearError: PropTypes.func.isRequired,
   error: PropTypes.shape({}),
   hideErrors: PropTypes.bool,
-  onClearError: PropTypes.func,
   onSubmit: PropTypes.func,
   onSubmitFailure: PropTypes.func,
   onSubmitSuccess: PropTypes.func,
@@ -129,7 +118,6 @@ MailingForm.propTypes = {
 MailingForm.defaultProps = {
   error: null,
   hideErrors: false,
-  onClearError: null,
   onSubmit: null,
   onSubmitFailure: null,
   onSubmitSuccess: null,
